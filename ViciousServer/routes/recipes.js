91e49a411f72f42a1ad9f8bb2fe83d41f57634cd
aquan <oreomilkenterprise1@gmail.com>
2017-04-07 19:20:12 -0500

    Added server.

--- ViciousServer/routes/recipes.js
+++ ViciousServer/routes/recipes.js
@@ -6,7 +6,7 @@ var express = require('express');
 var router = express.Router();
 
 var monk = require('monk');
-var db = monk('localhost:27017/vidzy');
+var db = monk('ds145790.mlab.com:45790/vidzy');
 
 router.get('/', function(req, res) {
     var collection = db.get('recipes');
