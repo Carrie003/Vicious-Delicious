8c02b511529375b62ba7bdfd35a1c8f687ab10fc
tianyoul <youzi323@live.cn>
2017-04-19 15:10:34 -0500

    Change the provider so it does not get recipes from the server every time we go into recipe-collection

--- viciousDelicious/src/providers/recipes.ts
+++ viciousDelicious/src/providers/recipes.ts
@@ -8,10 +8,12 @@ import 'rxjs/add/operator/map';
   See https://angular.io/docs/ts/latest/guide/dependency-injection.html
   for more info on providers and Angular 2 DI.
 */
+
 @Injectable()
 export class Recipes {
 
   shared: any;
+  gfy:any;
 
   constructor(public http: Http) {
     this.shared = null;
@@ -20,7 +22,9 @@ export class Recipes {
 
   loadRecipes(){
 
-    return new Promise(resolve => {
+    if(!this.shared){
+
+      return new Promise(resolve => {
       this.http.get('https://viciousdelicious.herokuapp.com/api/recipes')
         .map(res => res.json())
         .subscribe(shared => {
@@ -29,7 +33,7 @@ export class Recipes {
         });
     });
 
-  }
+  }}
 
   loadBeverage(){
 
@@ -78,7 +82,17 @@ export class Recipes {
 
       .subscribe(res => {
         console.log(res.json());
+
       });
+
+    return new Promise(resolve => {
+      this.http.get('https://viciousdelicious.herokuapp.com/api/recipes')
+        .map(res => res.json())
+        .subscribe(shared => {
+          this.shared = shared;
+          resolve(this.shared);
+        });
+    });
 
   }
 
@@ -87,6 +101,60 @@ export class Recipes {
     this.http.delete('https://viciousdelicious.herokuapp.com/api/recipes' + id).subscribe((res) => {
       console.log(res.json());
     });
+
+  }
+
+  imgurAPI(path){
+    let link=null;
+    let response=null;
+    let headers = new Headers();
+
+    headers.append('Authorization', 'Bearer 878ad20c4f11e3f308cfb85838e19d26ad96e0ed');
+    //headers.append('Authorization', 'Client-ID 990bf56cfa4922a');
+    //this.http.post('https://api.gfycat.com/v1/gfycats', {headers: headers})
+    //  .subscribe(res => {
+    //    response=res;
+    //    console.log("get title");
+    //  });
+
+    // this.http.post('https://filedrop.gfycat.com', {
+    //  title:response.gfyname,
+    //  fetchUrl: path,
+    //  captions: [
+    //    {
+    //      startSeconds:0,
+    //      duration: 5,
+    //      text:response.secret
+    //    }
+    //  ]
+    // })
+    //  .subscribe(res => {
+    //    console.log("Converting to GIF.");
+    //  });
+
+
+    this.http.post('https://api.imgur.com/3/image', {
+      headers:{
+        "Authorization":"client-id 990bf56cfa4922a",
+        'Content-Type': 'application/json'
+      },
+      image: path,
+      title: path,
+    })
+      .subscribe(res => {
+        console.log(res.json());
+        console.log("Posting Img to Imgur");
+        response=res;
+      });
+
+
+    //headers.append('Authorization', 'Bearer <<eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE0OTIyMDQ2NjgsImlzcyI6IjJfVlNGc19BIiwicm9sZXMiOlsiQ29udGVudF9SZWFkZXIiXX0.jZqPRhltX6_XLAepIYgsp_Fmud3jklIWkc3K9CQwmBw>>');
+    // this.http.get("https://api.imgur.com/3/image/"+response.id,{headers:headers})
+    //   .subscribe(res => {
+    //     link=res;
+    //     console.log("Get the GIF back.");
+    //     return link.link;
+    //   });
 
   }
 }
